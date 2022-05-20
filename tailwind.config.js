f8a00b737b4dd910b6142904010aca5b76085cd4
refaldy04 <refaldybanggana@gmail.com>
2022-05-20 15:43:09 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    theme: {
+      container: {
+        center: true,
+        padding: '16px',
+      },
+    },
+    extend: {
+      colors: {
+        primary: '#0284c7',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screen: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
